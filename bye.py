b156a845609e7b7f7a37f618405d9838045db027
mahmoud-Mohamed1492 <138544665+mahmoud-Mohamed1492@users.noreply.github.com>
2023-10-23 20:08:22 +0300

    Add files via upload

---
+++ bye.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import webbrowser
+st.markdown("""
+<div class = 'container'>
+  <p class = 'title'>GOOD LUCK</p>
+  <p class = 'speech'>Follow up Your E-mail \n\nYour task and interview date will send into it</p>                      
+</div> 
+
+<style>
+.container{
+        width: 900px;
+    border: 3px solid black;
+    padding: 30px;
+    margin: 0px 0px 30px -100px;
+    border-radius: 15px;
+    background: white;   
+}
+            
+.title{
+        text-align: center; 
+        font-family:Cooper Black;
+        color:#000000;
+        font-size: 50px; 
+}                
+</style>            
+
+""" , unsafe_allow_html=True)
+
+
